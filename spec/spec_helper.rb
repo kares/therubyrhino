244611be1c577e80aef48c7f61e7538cda33d8d2
kares <self@kares.org>
2019-06-19 14:54:56 +0200

    a more predictable rspec backtrace cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,4 +36,9 @@ RSpec.configure do |config|
   config.filter_run_excluding :compat => /(0.5.0)|(0.6.0)/ # RedJS
   config.include Rhino::SpecHelpers
   config.deprecation_stream = 'spec/deprecations.log'
+
+  config.backtrace_clean_patterns = [
+    /gems\//,
+    /lib\/rspec\/(core|expectations|matchers|mocks)/,
+  ]
 end
